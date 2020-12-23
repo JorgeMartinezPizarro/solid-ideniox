3b1082deddb9a9335cdaf57e67390b9eda85c5fe
Jorge Martínez Pizarro <jorge.martinez.pizarro@eccenca.com>
2020-12-23 18:42:12 +0100

    use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
+
 import Login from './components/Login.js'
 import Read from './components/Read.js'
 import Write from './components/Write.js'
 import Nicks from './components/Nicks';
 import Demo from './components/Demo';
-import { Button } from "@blueprintjs/core";
+
 import './App.css';
 
 function App() {
