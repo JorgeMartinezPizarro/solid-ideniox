64d4860fde83eea73083074ae743f2ab9c4a584c
Jorge Martínez Pizarro <jorge.martinez.pizarro@eccenca.com>
2021-01-10 21:07:10 +0100

    layout changes

--- src/components/Thing.js
+++ src/components/Thing.js
@@ -2,34 +2,46 @@ import React, {useState, useEffect} from 'react';
 
 import { Table, Container, Row, Spinner, Button } from 'react-bootstrap';
 
+import _ from 'lodash';
+
 import {getResource} from "../api/things";
 
+import {getWebId} from '../api/explore'
 
 export default () => {
 
     const [profile, setProfile] = useState([])
     const [error, setError] = useState({})
-
     const [value, setValue] = useState('')
 
-    useEffect(async ()=>{
-        //setProfile(await getResource(va))
-    }, [])
+    useEffect(() => {
+        const x = async () => {
+            const webID = await getWebId();
+            setValue(webID)
+        }
+        x()
+    })
 
     return <Container>
         <Table>
+            <thead>
+                <tr>
+                    <th>Graph</th>
+                    <th>Subject</th>
+                    <th>Predicate</th>
+                    <th>Object</th>
+                </tr>
+            </thead>
             <tbody>
             <tr>
-                <td></td>
-                <td></td>
-                <td><input type={'text'} value={value} onChange={e=>{setValue(e.target.value)}} /></td>
+                <td colSpan={3}><input style={{width: '100%'}} type={'text'} value={value} onChange={e=>{setValue(e.target.value)}} /></td>
                 <td><Button onClick={async()=> {
                     const a = await getResource(value)
                     setProfile(a.values)
                     setError(a.error)
                 }}>Load</Button></td>
             </tr>
-            {error && <tr><td>{JSON.stringify(error, null, 2)}</td><td></td><td></td><td></td></tr>}
+            {!_.isEmpty(error) && <tr><td>{JSON.stringify(error, null, 2)}</td><td></td><td></td><td></td></tr>}
             {profile.map(p=> {
                 return <tr>
                     <td>{p.graph}</td>
