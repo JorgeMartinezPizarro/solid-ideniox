4893b426e61c282da3c6b86d8f5af2c6633a35a6
Jorge Martínez Pizarro <jorge.martinez.pizarro@eccenca.com>
2021-01-22 00:06:00 +0100

    show images

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -33,53 +33,55 @@ export default () => {
         });
 
 
-        return x ? x.photo : '';
+        return x ? x.photo : '/favicon.png';
     }
 
     return <Container key={'x'}>
-        <Button onClick={() => setSend(!send)}>{send?'Inbox':"Redact"}</Button>
-        <Button onClick={() => getNotifications().then(setNotifications)}>Reload</Button>
-        {send && <input type={'text'} value={title} style={{width: '100%'}} onChange={e=> setTitle(e.target.value)} />}
-        {send && <textarea type={'text'} value={text} style={{height: '400px', width: '100%'}} onChange={e=> setText(e.target.value)} />}
-        {send && <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                {selectedInbox.name || 'Select an user'}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {_.map(inboxes, inbox => <Dropdown.Item onClick={() => setSelectedInbox(inbox)}>{inbox.name}</Dropdown.Item>)}
-            </Dropdown.Menu>
-        </Dropdown>}
-        {send && <Button disabled={!title || !text || !selectedInbox} onClick={async () => {
-            await sendNotification(text, title, selectedInbox.url, selectedInbox.inbox)
-            setText('');
-            setTitle('');
-        }}>Send</Button>}
-        {!send && <Table><tbody>
+        <Table><tbody>
             <tr>
                 <td ></td>
                 <td style={{width: '170px'}}></td>
             </tr>
-            {notifications.map(notification => {
-                console.log(notification)
-
-                return <tr className={notification.read === 'false' ? 'unread-message message' : 'message'}>
-                    <td>
-                        <img className='image-chat' src={getFoto(notification.user)}/>
-                        {!notification.destinatary && <span className={'image-chat'}><span className="material-icons">visibility</span></span>}
-                        {notification.destinatary && <img className='image-chat' src={getFoto(notification.destinatary)}/>}
-                        {notification.text}
-                    </td>
-                    <td>
-                        <Button onClick={async () => {
-                            await markNotificationAsRead(notification.url);
-                            setNotifications(await getNotifications());
-                        }}><span className="material-icons">visibility</span></Button>
-                        <Button variant='danger' onClick={async () => {
-                            await deleteNotification(notification.url);
-                            setNotifications(await getNotifications());
-                        }}><span className="material-icons">delete</span></Button>
-                    </td>
+            <tr><td colSpan={2}><Button onClick={() => setSend(!send)}><span className="material-icons">{send ? 'list' : 'edit'}</span></Button><Button onClick={() => getNotifications().then(setNotifications)}><span className="material-icons">refresh</span></Button></td></tr>
+            {send && <input type={'text'} value={title} style={{width: '100%'}} onChange={e=> setTitle(e.target.value)} />}
+            {send && <textarea type={'text'} value={text} style={{height: '400px', width: '100%'}} onChange={e=> setText(e.target.value)} />}
+            {send && <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {selectedInbox.name || 'Select an user'}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {_.map(inboxes, inbox => <Dropdown.Item onClick={() => setSelectedInbox(inbox)}>{inbox.name}</Dropdown.Item>)}
+                </Dropdown.Menu>
+            </Dropdown>}
+            {send && <Button disabled={!title || !text || !selectedInbox} onClick={async () => {
+                await sendNotification(text, title, selectedInbox.url, selectedInbox.inbox)
+                setText('');
+                setTitle('');
+            }}>Send</Button>}
+            {!send && <>
+                <tr>
+                    <td ></td>
+                    <td style={{width: '170px'}}></td>
+                </tr>
+                {notifications.map(notification => {
+                    return <tr className={notification.read === 'false' ? 'unread-message message' : 'message'}>
+                        <td>
+                            <img className='image-chat' src={getFoto(notification.user)}/>
+                            {<img className='image-chat' src={getFoto(notification.destinatary)}/>}
+                            {notification.text}
+                        </td>
+                        <td>
+                            <Button onClick={async () => {
+                                await markNotificationAsRead(notification.url);
+                                setNotifications(await getNotifications());
+                            }}><span className="material-icons">visibility</span></Button>
+                            <Button variant='danger' onClick={async () => {
+                                await deleteNotification(notification.url);
+                                setNotifications(await getNotifications());
+                            }}><span className="material-icons">delete</span></Button>
+                        </td>
                 </tr>})}
-        </tbody></Table>}
+            </>}
+        </tbody></Table>
     </Container>
 }
