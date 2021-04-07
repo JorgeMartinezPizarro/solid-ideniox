916c215d28c79175f0a30ab64fec20bca80f04a9
Jorge Martínez Pizarro <jorge.martinez.pizarro@eccenca.com>
2021-04-07 21:14:02 +0200

    style fullscreen images

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,22 +5,29 @@ export default Image = (props) => {
     const {url} = props;
 
     const [content, setContent] = useState(undefined);
-
+    const [fullscreen, setFullscreen] = useState(false);
     useEffect(() => {
         readFile(url).then(content => {
             if (typeof content === 'object') {
                 const urlCreator = window.URL || window.webkitURL;
                 const imageUrl = urlCreator.createObjectURL(content);
-                setContent(<img style={{width: '100%', height: 'auto'}} src={imageUrl} />);
+                setContent(<img
+                    style={{width: '100%', height: 'auto', cursor: "pointer"}}
+                    src={imageUrl}
+                />);
             }
         })
 
     }, [])
 
+
+    console.log(fullscreen)
     if (content === undefined) {
         return <div>Loading</div>
 
     }
-    else return content;
+    else return <div onClick={() => setFullscreen(!fullscreen)} className={fullscreen === true ? 'fullscreen-image' : ''}>
+        {content}
+    </div>;
 
 }
